63799701c23952ffdfc8f858a4388fdf5c1ccef7
BAKEZQ <zhongquan789@gmail.com>
2020-10-12 23:31:41 +0800

    Update hic2sci.sh

--- scripts/hic2sci.sh
+++ scripts/hic2sci.sh
@@ -8,7 +8,7 @@ for ((i=1;i<=22;i++)); do
 	for ((j=$((i))+1;j<=22;j++)); do 
 		c1=chr${i}
 		c2=chr${j}
-		java -jar ${JUICERTOOLS} dump observed KR ${infile} chr${i} chr${j}  BP ${res} | awk -v c1=chr${i} -v c2=chr${j} -v res=${res} '{OFS="\t"}{print c1,$1,$1+res,c2,$2,$2+res,$3}' >> ${outfile};
+		java -jar ${JUICERTOOLS} dump observed GW_KR ${infile} chr${i} chr${j}  BP ${res} | awk -v c1=chr${i} -v c2=chr${j} -v res=${res} '{OFS="\t"}{print c1,$1,$1+res,c2,$2,$2+res,$3}' >> ${outfile};
 	done 
 done
 
